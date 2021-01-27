0e98361e1360859a6dde1fa8d3cfb3c23c8d0607
Reem-mobaideen <mubaideen.reem@gmail.com>
2021-01-27 15:24:10 +0200

    css font command

---
+++ app.js
@@ -0,0 +1,18 @@
+console.log ('Hello');
+var userName='Reem';
+console.log (userName);
+alert('welcome in CSS');
+var message=' '
+var FontWeight= prompt ('How would the font shape look like?');
+console.log (FontWeight);
+if (FontWeight= 'normal') {
+    message= 'The font shape is normal'
+
+}
+else if (FontWeight= 'bold'){
+    message= 'The font shape is thick'
+}
+else {
+    message= 'The font shape is thicker'
+}
+document.write (message);
