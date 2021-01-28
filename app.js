c4867a91751bc6974317ffc596d538af08b29638
Reem-mobaideen <mubaideen.reem@gmail.com>
2021-01-28 15:56:59 +0200

    language code

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 console.log('Hello');
 var userName = 'Reem';
-console.log(userName);
+//console.log(userName);
 //alert('welcome in CSS');
 //var message = ' '
+
+
 alert('programming languages option');
 var level= prompt('what is your progmming level')
-console.log(level)
+
 
 if (level=="junior"){
     document.write(' you are a junior programmer')
@@ -16,24 +18,38 @@ else if (level == 'senior'){
     document.write('this page for programming learn')
 }
 
-var language=' ';
 
-while (option ==='HTML' && option==='CSS'){
+var language=' ';
+if (level === 'junior'){
+    language='<img src="html.png"/>';
+    console.log(language);
+} else if (level === 'senior'){
+    language= '<img src="CSS.png"/>';
+}
+
+document.write(language);
+var result= ' '
+
+result=language;
+
+document. write(result);
+
+/*while (option ==='HTML' && option==='CSS'){
     option= prompt('please choose either HTML or CSS')
 }
 
 if (option === 'HTML'){
-    language =' <img src="CODING-SCHOOL/HTML.jfif"/>'
+    language =' <img src="CODING-SCHOOL/html.png"/>'
 } else if (option ==="CSS"){
     language='<img src="CODING-SCHOOL/CSS.png/>'
 }
 
 
 var message=' ';
-var result= ' ';
+var result= ' '; */
 
 var option = prompt (' do you want HTML or CSS language?');
-console.log(userName);
+
 
 /* var FontWeight = prompt('How would the font shape look like?');
 console.log(FontWeight);
@@ -45,9 +61,8 @@ if (FontWeight == 'normal') {
 } else {
     message = 'The font shape is thicker'
 }   */
-document.write('<h2>' + message +'</h2>');    
-document.write(result);
-
+/* document.write('<h2>' + message +'</h2>');    
+document.write(result);*/
 
 
 
