b4872f1d9e66fb8df6d636e02bb3a0803d5066aa
Reem-mobaideen <mubaideen.reem@gmail.com>
2021-01-28 14:13:45 +0200

    choose language

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 console.log('Hello');
 var userName = 'Reem';
 console.log(userName);
-alert('welcome in CSS');
-var message = ' '
-var FontWeight = prompt('How would the font shape look like?');
+//alert('welcome in CSS');
+//var message = ' '
+alert('programming languages option');
+
+var language=' ';
+
+while (option ==='HTML' && option==='CSS'){
+    option= prompt('please choose either HTML or CSS')
+}
+
+if (option === 'HTML'){
+    language =' <img src="CODING-SCHOOL/HTML.jfif"/>'
+} else if (option ==="CSS"){
+    language='<img src="CODING-SCHOOL/CSS.png/>'
+}
+var message=' ';
+var result= ' ';
+var option = prompt (' do you want HTML or CSS language?');
+console.log(userName);
+
+/* var FontWeight = prompt('How would the font shape look like?');
 console.log(FontWeight);
 if (FontWeight == 'normal') {
     message = 'The font shape is normal'
@@ -12,5 +30,12 @@ if (FontWeight == 'normal') {
     message = 'The font shape is thick'
 } else {
     message = 'The font shape is thicker'
-}
-document.write(message);
+}   */
+document.write('<h2>' + message +'</h2>');    
+document.write(result);
+
+
+
+
+
+
